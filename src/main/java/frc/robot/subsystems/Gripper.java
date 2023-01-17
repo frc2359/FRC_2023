6d7d2b53ac412a970bd889958a37f370a5e1fa85
Siven Panda <68242448+sivepanda@users.noreply.github.com>
2023-01-17 17:17:24 -0600

    hghgfg

--- src/main/java/frc/robot/subsystems/Gripper.java
+++ src/main/java/frc/robot/subsystems/Gripper.java
@@ -1,16 +1,41 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.math.controller.PIDController;
 import static frc.robot.RobotMap.*;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 public class Gripper {
-    private CANSparkMax extMotor0 = new CANSparkMax(GRIPPER_0, MotorType.kBrushless);
-    private CANSparkMax extMotor1 = new CANSparkMax(GRIPPER_1, MotorType.kBrushless);
-    private CANSparkMax extMotors[] = new CANSparkMax[]{extMotor0, extMotor1};
-    PIDController pid = new PIDController(0.00000000001, 0.00000000001, 0.00000000001);
+    
+    private boolean activated = false;
+    private static WPI_VictorSPX gripDevice = new WPI_VictorSPX(8);
+    private static Joystick buttons = new Joystick(0);
+    private static DigitalInput limSwitch = new DigitalInput(0);
 
+
+    public void gripEm(){
+        
+        if(buttons.getRawButtonPressed(8)){
+            activated = true;
+            while(limSwitch.get() == false){
+            gripDevice.set(.6);
+            }
+        } 
+        if(buttons.getRawButtonPressed(10)) {
+            activated = true;
+            gripDevice.set(.6);
+        } 
+        if(buttons.getRawButtonPressed(12)){
+            activated = false;
+        }
+        if(!activated){
+            gripDevice.set(0);
+        }
+    }
     
 
 }
