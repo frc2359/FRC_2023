307872a55be82dce5271be7069da707083a89600
sivepanda <sivenpanda@icloud.com>
2023-01-21 01:40:52 -0600

    swervy things

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,23 +1,48 @@
 package frc.robot;
 
-public interface RobotMap {
-    public static final int GRIPPER_0 = 8;
-    public static final int GRIPPER_1 = 9;
-    public static final int GRIPPER_2 = 10;
+import com.swervedrivespecialties.swervelib.SwerveModule;
+import com.swervedrivespecialties.swervelib.Mk4iSwerveModuleHelper.GearRatio;
+import com.swervedrivespecialties.swervelib.Mk4iSwerveModuleHelper;
 
-    // drive
+public interface RobotMap {
+
+    /* ---------------------------------- IO & HANDLING --------------------------------- */
     public static final int DRIVE_PORT = 0; //USB IO Port
     public static final double TURN_SPEED_MULT = 1;
     public static final double DRIVE_SPEED_MULT = 1;
 
     public static final boolean BRAKE_MODE_DRIVE = true;
 
+
+
+    /* -------------------------------------------------------------------------- */
+    /*                                   GRIPPER                                  */
+    /* -------------------------------------------------------------------------- */
+
+    /* ---------------------------- GRIPPER MOTOR ID ---------------------------- */
+    public static final int GRIPPER_0 = 8;
+    public static final int GRIPPER_1 = 9;
+    public static final int GRIPPER_2 = 10;
+
+    
+
+    /* -------------------------------------------------------------------------- */
+    /*                                 BASIC DRIVE                                */
+    /* -------------------------------------------------------------------------- */
     public static final int DRIVE_1 = 1;
     public static final int DRIVE_2 = 2;
     public static final int DRIVE_3 = 3;
     public static final int DRIVE_4 = 4;
 
-    // Swerve motors
+
+
+    /* -------------------------------------------------------------------------- */
+    /*                                SWERVE DRIVE                                */
+    /* -------------------------------------------------------------------------- */
+
+    public static final GearRatio SWERVE_GEAR_RATIO = Mk4iSwerveModuleHelper.GearRatio.L1;
+
+    /* --------------------------- SWERVE MOTOR ID --------------------------- */
     public static final int FRONT_LEFT_DRIVE = 1;
     public static final int FRONT_LEFT_STEER = 2;
     public static final int FRONT_RIGHT_DRIVE = 3;
@@ -27,21 +52,20 @@ public interface RobotMap {
     public static final int BACK_RIGHT_DRIVE = 7;
     public static final int BACK_RIGHT_STEER = 8;
 
-    //Swerve encoder
+    /* -------------------------- SWERVE ENCODER PORTS -------------------------- */
     public static final int FRONT_LEFT_ENC = 1;
     public static final int FRONT_RIGHT_ENC = 2;
     public static final int BACK_LEFT_ENC = 3;
     public static final int BACK_RIGHT_ENC = 4;
 
-    // steer offset
+    /* -------------------------- SWERVE STEER OFFSETS -------------------------- */
     public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(238.97);
     public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(238.97);
     public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(238.97);
     public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(238.97);
 
-    // drive constants (measurements)
+    /* ----------------------------- DRIVE CONSTANTS ---------------------------- */
     public static final int DRIVE_TICKS_TO_ROTATION = 1250;
     public static final int DRIVETRAIN_TRACKWIDTH_METERS = 1;
     public static final int DRIVETRAIN_WHEELBASE_METERS = 1;
-
 }
--- src/main/java/frc/robot/subsystems/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -5,29 +5,92 @@ import static frc.robot.RobotMap.*;
 import java.lang.Math;
 
 import com.swervedrivespecialties.swervelib.SwerveModule;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.ctre.phoenix.sensors.PigeonIMU; // REPLACE WITH NAVX
 import com.swervedrivespecialties.swervelib.Mk4iSwerveModuleHelper;
 
 // EXAMPLE CODE: https://github.com/SwerveDriveSpecialties/swerve-lib-2022-unmaintained/blob/develop/examples/mk3-testchassis/src/main/java/com/swervedrivespecialties/examples/mk3testchassis/subsystems/DrivetrainSubsystem.java
 
-public class SwerveDrive {
-        private SwerveModule frontLeftModule;
-        private SwerveModule frontRightModule;
-        private SwerveModule backLeftModule;
-        private SwerveModule backRightModule;
-
+public class SwerveDrive extends SubsystemBase {
         private static final double MAX_VOLTAGE = 12.0;
         public static final double MAX_VELOCITY_METERS_PER_SECOND = 4.14528;
         public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND = MAX_VELOCITY_METERS_PER_SECOND /
         Math.hypot(DRIVETRAIN_TRACKWIDTH_METERS / 2.0, DRIVETRAIN_WHEELBASE_METERS / 2.0);
 
+        private SwerveModule frontLeftModule;
+        private SwerveModule frontRightModule;
+        private SwerveModule backLeftModule;
+        private SwerveModule backRightModule;
+
+        private final PigeonIMU gyroscope = new PigeonIMU(1); // REPLACE WITH NAVX
+
+        private final SwerveDriveKinematics kinematics = new SwerveDriveKinematics(
+            new Translation2d(DRIVETRAIN_TRACKWIDTH_METERS / 2.0, DRIVETRAIN_WHEELBASE_METERS / 2.0),
+            new Translation2d(DRIVETRAIN_TRACKWIDTH_METERS / 2.0, -DRIVETRAIN_WHEELBASE_METERS / 2.0),
+            new Translation2d(-DRIVETRAIN_TRACKWIDTH_METERS / 2.0, DRIVETRAIN_WHEELBASE_METERS / 2.0),
+            new Translation2d(-DRIVETRAIN_TRACKWIDTH_METERS / 2.0, -DRIVETRAIN_WHEELBASE_METERS / 2.0)
+    );
+
+        private final SwerveDriveOdometry odometry = new SwerveDriveOdometry(kinematics, Rotation2d.fromDegrees(gyroscope.getFusedHeading()), null);
+
+        private ChassisSpeeds chassisSpeeds = new ChassisSpeeds(0.0, 0.0, 0.0);
+
+        
+
         public SwerveDrive() {
                 frontLeftModule = Mk4iSwerveModuleHelper.createFalcon500Neo(
-                        Mk4iSwerveModuleHelper.GearRatio.L1,
+                        SWERVE_GEAR_RATIO,
                         FRONT_LEFT_DRIVE,
                         FRONT_LEFT_STEER,
                         FRONT_LEFT_ENC,
                         FRONT_LEFT_MODULE_STEER_OFFSET
                 );
 
+                frontRightModule = Mk4iSwerveModuleHelper.createFalcon500Neo(
+                        SWERVE_GEAR_RATIO,
+                        FRONT_RIGHT_DRIVE,
+                        FRONT_RIGHT_STEER,
+                        FRONT_RIGHT_ENC,
+                        FRONT_RIGHT_MODULE_STEER_OFFSET);
+
+                backLeftModule = Mk4iSwerveModuleHelper.createFalcon500Neo(
+                        SWERVE_GEAR_RATIO,
+                        BACK_LEFT_DRIVE,
+                        BACK_LEFT_STEER,
+                        BACK_LEFT_ENC,
+                        BACK_LEFT_MODULE_STEER_OFFSET);
+
+                backRightModule = Mk4iSwerveModuleHelper.createFalcon500Neo(
+                        SWERVE_GEAR_RATIO,
+                        BACK_RIGHT_DRIVE,
+                        BACK_RIGHT_STEER,
+                        BACK_RIGHT_ENC,
+                        BACK_RIGHT_MODULE_STEER_OFFSET);
+
         }
+
+        @Override
+    public void periodic() {
+        odometry.update(Rotation2d.fromDegrees(gyroscope.getFusedHeading()),
+                new SwerveModuleState(frontLeftModule.getDriveVelocity(), new Rotation2d(frontLeftModule.getSteerAngle())),
+                new SwerveModuleState(frontRightModule.getDriveVelocity(), new Rotation2d(frontRightModule.getSteerAngle())),
+                new SwerveModuleState(backLeftModule.getDriveVelocity(), new Rotation2d(backLeftModule.getSteerAngle())),
+                new SwerveModuleState(backRightModule.getDriveVelocity(), new Rotation2d(backRightModule.getSteerAngle()))
+        );
+
+        SwerveModuleState[] states = kinematics.toSwerveModuleStates(chassisSpeeds);
+
+        frontLeftModule.set(states[0].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[0].angle.getRadians());
+        frontRightModule.set(states[1].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[1].angle.getRadians());
+        backLeftModule.set(states[2].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[2].angle.getRadians());
+        backRightModule.set(states[3].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE, states[3].angle.getRadians());
+    }
 }
